8708309bfb44025d3103a6c0c82d7fe88321fed2
Brandon Levy <brandon.levy@mail.mcgill.ca>
2017-11-03 16:10:11 -0400

    Added choices and loops if performing multiple methods

--- exercise1.rb
+++ exercise1.rb
@@ -32,8 +32,35 @@ puts "What is Brandon's Interest Rate (%) ?"
 b=gets.chomp.to_i
 brandon = BankAccount.new(a, b)
 
-brandon.deposit=(40)
-brandon.deposit=(80)
-brandon.withdraw=(45)
-brandon.gain_interest
+def choice(account, n)
+  if n == "deposit"
+    puts "How much ($)"
+    d = gets.chomp.to_i
+    account.deposit=(d)
+  elsif n == "withdraw"
+    puts "How much ($)"
+    w = gets.chomp.to_i
+    account.withdraw=(w)
+  elsif n == "add interest"
+    account.gain_interest
+  end
+end
+
+def continue(account)
+  puts "Are you done?"
+  continue = gets.chomp
+  if continue == "no"
+    puts "Do you want to deposit, withdraw or add interest"
+    n = gets.chomp
+    choice(account, n)
+    continue(account)
+    else
+  end
+end
+
+puts "Do you want to deposit, withdraw or add interest"
+n = gets.chomp
+choice(brandon, n)
+continue(brandon)
+
 puts "Your balance is #{brandon.balance}$"
